6c6977fad460db223a7a311ea9d34db217119884
Aleksandr Salo <alexsalovrn@gmail.com>
2012-03-01 16:39:29 +0400

    df

--- Lab2_v7_stub/src/edu/calpoly/android/lab2/Joke.java
+++ Lab2_v7_stub/src/edu/calpoly/android/lab2/Joke.java
@@ -24,7 +24,8 @@ public class Joke {
 	 * UNRATED.
 	 */
 	public Joke() {
-		//TODO
+		m_strJoke = "";
+		m_nRating = UNRATED;
 	}
 	
 	/**
@@ -35,7 +36,8 @@ public class Joke {
 	 * 						joke.
 	 */
 	public Joke(String strJoke) {
-		//TODO
+		m_strJoke = strJoke;
+		m_nRating = UNRATED;
 	}
 	
 	/**
@@ -47,7 +49,8 @@ public class Joke {
 	 * @param dLike	Rating value to initialize the rating of this joke.
 	 */
 	public Joke(String strJoke, int nRating) {
-		//TODO
+		m_strJoke = strJoke;
+		m_nRating = nRating;
 	}
 	
 	/**
@@ -56,8 +59,7 @@ public class Joke {
 	 * @return	A String value containing the text of this joke.
 	 */
 	public String getJoke() {
-		//TODO
-		return null;
+		return m_strJoke;
 	}
 
 	/**
@@ -66,7 +68,7 @@ public class Joke {
 	 * @param strJoke	The text of this joke.
 	 */
 	public void setJoke(String mstrJoke) {
-		//TODO
+		m_strJoke = mstrJoke;
 	}
 
 	/**
@@ -76,8 +78,7 @@ public class Joke {
 	 * 			rating constants.
 	 */
 	public int getRating() {
-		//TODO
-		return 0;
+		return m_nRating;
 	}
 	
 	/**
@@ -86,7 +87,7 @@ public class Joke {
 	 * @param rating	One of the possible rating constants.
 	 */
 	public void setRating(int rating) {
-		//TODO
+		m_nRating = rating;
 	}
 	
 	/**
@@ -96,8 +97,7 @@ public class Joke {
 	 */
 	@Override
 	public String toString() {
-		//TODO
-		return null;
+		return m_strJoke;
 	}
 	
 	/**
@@ -111,7 +111,7 @@ public class Joke {
 	 */
 	@Override
 	public boolean equals(Object obj) {
-		//TODO
-		return true;
+		String s = obj.toString();
+		return  this.m_strJoke.equals(s);
 	}
 }
